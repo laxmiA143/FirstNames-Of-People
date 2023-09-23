ecb7a4fbdba5852510729687faa8bc6f315055a8
laxmiA143 <www.lakshmiadhiyaman@gmail.com>
2023-09-23 11:55:04 +0000

    solution

---
+++ names/index.js
@@ -0,0 +1,9 @@
+const a = require("../country/state/city/index");
+
+const b = require("../utilities/utils/index");
+
+const getPeopleInCity = (c) => {
+  return b(c);
+};
+
+module.exports = getPeopleInCity;
